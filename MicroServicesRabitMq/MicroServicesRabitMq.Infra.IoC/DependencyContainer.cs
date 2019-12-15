5a068afba815ab08b167fb2757bcc9e2e853fd71
Numan KIZILIRMAK <nmnkizilirmak@gmail.com>
2019-12-15 12:32:36 +0300

    Infra.IoC implemented

---
+++ MicroServicesRabitMq/MicroServicesRabitMq.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using MicroServicesRabitMq.Domain.Core.Bus;
+using MicroServicesRabitMq.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroServicesRabitMq.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabitMqBus>();
+        }
+    }
+}
