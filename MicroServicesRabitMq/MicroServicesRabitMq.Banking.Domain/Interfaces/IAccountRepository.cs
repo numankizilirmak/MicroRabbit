b5a5b6d4f6e26a1a041b822697bb0b632d3b0fa8
Numan KIZILIRMAK <nmnkizilirmak@gmail.com>
2019-12-16 19:56:25 +0300

    Banking.Domain account interface and class

---
+++ MicroServicesRabitMq/MicroServicesRabitMq.Banking.Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using MicroServicesRabitMq.Banking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroServicesRabitMq.Banking.Domain.Interfaces
+{
+    public interface IAccountRepository
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
---
+++ MicroServicesRabitMq/MicroServicesRabitMq.Banking.Domain/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MicroServicesRabitMq.Banking.Domain.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string AccountType { get; set; }
+
+        public decimal AccountBalance { get; set; }
+    }
+}
